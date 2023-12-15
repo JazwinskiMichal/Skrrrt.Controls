29f0a97bc5b4ee0dd467c494fab01bab5db8f4c7
Michal Jazwinski <mail@mihow.pl>
2023-12-15 13:04:31 +0100

    Add project files.

---
+++ Skrrrt.Controls/RippleButton.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Skrrrt.RippleButton
+{
+    /// <summary>
+    /// Interaction logic for RippleButton.xaml
+    /// </summary>
+    public partial class RippleButton : Button
+    {
+        private Storyboard _storyboard;
+        private Point _mousePosition;
+        private Ellipse _ellipse;
+
+        public RippleButton()
+        {
+            InitializeComponent();
+
+            // Get storyboard from RippleButton
+            _storyboard = (Storyboard)this.Resources["RippleEffect"];
+        }
+
+        private async void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Canvas _canvas = (Canvas)sender as Canvas;
+            if (_canvas != null)
+            {
+                // e.StylusDevice.Capture(_canvas);
+
+                _mousePosition = e.GetPosition(_canvas);
+
+                // Get the ellipse from the canvas
+                _ellipse = _canvas.Children[1] as Ellipse;
+
+                // Move the ellipse to the point where the mouse was clicked
+                _ellipse.SetValue(Canvas.LeftProperty, _mousePosition.X - _ellipse.ActualWidth / 2);
+                _ellipse.SetValue(Canvas.TopProperty, _mousePosition.Y - _ellipse.ActualHeight / 2);
+
+                await Task.Delay(100);
+
+                // Attach storyboard to the ellipse
+                Storyboard.SetTarget(_storyboard, _ellipse);
+                _storyboard.Begin();
+            }
+        }
+
+        private void Storyboard_Completed(object sender, EventArgs e)
+        {
+            _storyboard.Remove();
+        }
+    }
+}
